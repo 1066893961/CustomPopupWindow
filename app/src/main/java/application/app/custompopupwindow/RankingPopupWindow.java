4145c928523021e090d5000c7ca2815d74366143
lwz <m18911450394@163.com>
2018-03-06 17:08:08 +0800

    test merge

--- app/src/main/java/application/app/custompopupwindow/RankingPopupWindow.java
+++ app/src/main/java/application/app/custompopupwindow/RankingPopupWindow.java
@@ -20,6 +20,9 @@ import java.util.List;
  */
 
 public class RankingPopupWindow extends PopupWindow {
+    /**
+     * test
+     */
     private ListPopupWindowAdapter mListPopupWindowAdapter;
     private Listener mListener;
 
